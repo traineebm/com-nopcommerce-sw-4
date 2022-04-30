2671d3329e9902f5de5bd0e020e094b89f06a173
Primetraineesbm <bindi.maniar@gmail.com>
2022-05-01 00:28:46 +0100

    Second update 01/05 - 00:30

--- src/main/java/com/nopcommerce/demo/pages/HomePage.java
+++ src/main/java/com/nopcommerce/demo/pages/HomePage.java
@@ -6,6 +6,13 @@ import org.openqa.selenium.By;
 public class HomePage extends Utility {
     By loginLink = By.linkText("Log in");
     By registerLink = By.linkText("Register");
+    By computers = By.linkText("Computers");
+    By electronics = By.linkText("Electronics");
+    By apparel = By.linkText("Apparel");
+    By digitalDownloads = By.linkText("Digital downloads");
+    By books = By.linkText("Books");
+    By jewelry = By.linkText("Jewelry");
+    By giftcards = By.linkText("Gift Cards");
 
     public void clickOnLoginLink(){
         clickOnElement(loginLink);
@@ -13,5 +20,33 @@ public class HomePage extends Utility {
 
     public void clickOnRegisterLink(){
         clickOnElement(registerLink);
+    }
+
+    public void clickOnComputersTab(){
+        clickOnElement(computers);
+    }
+
+    public void clickOnElectronicsTab(){
+        clickOnElement(electronics);
+    }
+
+    public void clickOnApparelTab(){
+        clickOnElement(apparel);
+    }
+
+    public void clickOnDigitalDownloadsTab(){
+        clickOnElement(digitalDownloads);
+    }
+
+    public void clickOnBooksTab(){
+        clickOnElement(books);
+    }
+
+    public void clickOnJewelryTab(){
+        clickOnElement(jewelry);
+    }
+
+    public void clickOnGiftCardsTab(){
+        clickOnElement(giftcards);
     }
 }
